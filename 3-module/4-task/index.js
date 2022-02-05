e5d4823c94ad51819dea2e3314c7ca917cd090c3
Igor Tsuker <igorek888rane@gmail.com>
2022-02-05 22:11:21 +0300

    'showSalary'

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,8 @@
+
 function showSalary(users, age) {
-  // ваш код...
+  let nameAndSalary = users
+  .filter(user =>user.age <= age)
+  .map(user => `${user.name}, ${user.balance}`);
+  return nameAndSalary.join('\n');
 }
+showSalary(users,30)
