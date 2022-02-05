5c2d36f964d2b1131e1a1fb832ea8f92dc048931
Igor Tsuker <igorek888rane@gmail.com>
2022-02-05 20:38:55 +0300

    'camelize'

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  let arrStr = str.split('-');
+  const oneEl = arrStr[0];
+  let arrUp = arrStr
+  .filter(el => el !==arrStr[0])
+  .map(el => el[0].toUpperCase() + el.slice(1));
+  arrUp.unshift(oneEl);
+  return arrUp.join("");
+
 }
+console.log(camelize('list-style-image'));
