a1cf9023562d9555a7a2050d3492b5648138813b
Igor Tsuker <igorek888rane@gmail.com>
2022-02-10 14:48:38 +0300

    highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,26 +1,26 @@
 function highlight(table) {
-  let trEl = table.querySelectorAll("tbody>tr");
+  const  trEl = table.querySelectorAll("tbody>tr");
   for (let i = 0; i < trEl.length; i++) {
-    let statusColumn = trEl[i].cells[3];
-    let genderColumn = trEl[i].cells[2];
-    let ageColumn = trEl[i].cells[1];
+    const statusColumn = trEl[i].cells[3];
+    const  genderColumn = trEl[i].cells[2];
+    const ageColumn = trEl[i].cells[1];
+
     if (statusColumn.dataset.available === "true") {
       trEl[i].classList.add("available");
-    }
-    if (statusColumn.dataset.available === "false") {
+    }else if (statusColumn.dataset.available === "false") {
       trEl[i].classList.add("unavailable");
-    }
-    if (statusColumn.hasAttribute("data-available") === false) {
+    }else if (statusColumn.hasAttribute("data-available") === false) {
       trEl[i].setAttribute("hidden", "");
     }
-    if (Number(ageColumn.innerHTML) < 18) {
-      trEl[i].style.textDecoration = "line-through";
-    }
+    
     if (genderColumn.innerHTML === "m") {
       trEl[i].classList.add("male");
-    }
-    if (genderColumn.innerHTML === "f") {
+    }else if (genderColumn.innerHTML === "f") {
       trEl[i].classList.add("female");
+    }
+
+     if (+ageColumn.innerHTML < 18) {
+      trEl[i].style.textDecoration = "line-through";
     }
   }
 }
