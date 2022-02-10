0e3c160fc3c40d2aec5245030ad717c633419a58
Igor Tsuker <igorek888rane@gmail.com>
2022-02-10 14:38:55 +0300

    highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  let trEl = table.querySelectorAll("tbody>tr");
+  for (let i = 0; i < trEl.length; i++) {
+    let statusColumn = trEl[i].cells[3];
+    let genderColumn = trEl[i].cells[2];
+    let ageColumn = trEl[i].cells[1];
+    if (statusColumn.dataset.available === "true") {
+      trEl[i].classList.add("available");
+    }
+    if (statusColumn.dataset.available === "false") {
+      trEl[i].classList.add("unavailable");
+    }
+    if (statusColumn.hasAttribute("data-available") === false) {
+      trEl[i].setAttribute("hidden", "");
+    }
+    if (Number(ageColumn.innerHTML) < 18) {
+      trEl[i].style.textDecoration = "line-through";
+    }
+    if (genderColumn.innerHTML === "m") {
+      trEl[i].classList.add("male");
+    }
+    if (genderColumn.innerHTML === "f") {
+      trEl[i].classList.add("female");
+    }
+  }
 }
